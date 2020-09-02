4a48a3240e166ef80f5c103ef424784e0d9be3ba
fanjy <fanjiayu-kazuo>
2020-09-02 23:42:59 +0800

    add home network

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,9 +8,23 @@
 
 <script>
   import NavBar from'components/common/navbar/NavBar.vue'
+  import {getHomeMultidata} from '../../network/Home.js'
   export default{
+    data(){
+      return {
+        banners:[],
+        recommends:[],
+      }
+    },
     components:{
       NavBar
+    },
+    created() {
+      getHomeMultidata().then( res=>{
+        this.banners = res.data.banner;
+        this.recommends = res.data.recommend;
+      })
+
     }
   }
 </script>
